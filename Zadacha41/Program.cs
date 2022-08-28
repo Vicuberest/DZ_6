136a74cdf792adf8849d6376a524e683faa582ab
Victoria <vica-genrietka@yandex.ru>
2022-08-29 01:51:37 +0300

    Zadacha41

---
+++ Zadacha41/Program.cs
@@ -0,0 +1,18 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.WriteLine("Введите количество М чисел: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите сами числа: ");
+
+int [] array = new int [size];
+int count=0;
+
+for (int i = 0; i < size; i++)
+{
+    
+    array [i] = Convert.ToInt32(Console.ReadLine());
+    if ( array [i] > 0 ) count++;
+
+}
+Console.WriteLine($"Количество чисел, которые больше нуля = {count} ");
