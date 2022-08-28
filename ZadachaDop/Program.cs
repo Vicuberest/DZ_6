5d46e6f61ea01563b5d866f63eeb3f0a839dedcc
Victoria <vica-genrietka@yandex.ru>
2022-08-29 01:53:11 +0300

    ZadachaDop

---
+++ ZadachaDop/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+
+
+Console.WriteLine("Введите число: ");
+int decimalNumber = Convert.ToInt32(Console.ReadLine());
+
+string binNumber = Convert.ToString(decimalNumber, 2);
+Console.WriteLine(binNumber);
+
+// Метод решения подглядела тут :)) 
+// https://www.cyberforum.ru/csharp-beginners/thread389391.html
